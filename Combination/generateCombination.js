64bafcc9364c0f085b07658f9c2ec8cfa8fc8a10
Temi Makinde <temi.makinde@gmail.com>
2019-01-22 16:26:28 +0000

    Combination algorithm implemented

---
+++ Combination/generateCombination.js
@@ -0,0 +1,8 @@
+/*
+This function will generate different unique combination
+*/
+function generateCombination(list){
+    return list.reduce(function permute(res, item, key, arr) {
+        return res.concat(arr.length > 1 && arr.slice(0, key).concat(arr.slice(key + 1)).reduce(permute, []).map(function(perm) { return [item].concat(perm); }) || item);
+    }, []);
+}
