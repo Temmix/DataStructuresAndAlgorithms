7b6a143d131d9321abdf1659e7a6b5552eb537b2
Temi Makinde <temi.makinde@gmail.com>
2019-02-01 17:17:51 +0000

    Create bubbleSort.ts

---
+++ Algorithms/bubbleSort.ts
@@ -0,0 +1,26 @@
+function bubbleSort(list){ 
+   for(let i = 0; i < list.length; i++){ 
+      for(let j = i + 1; j < list.length; j++){ 
+          if(list[i] > list[j]){
+              let temp = list[i];
+              list[i] = list[j];
+              list[j] = temp;
+          }
+      }
+   }  return list;
+}
+
+function bubbleSort2(array) { 
+    for (var i = array.length; i > 0; i--) { 
+      for (var j = 0; j < i; j++) { 
+        if (array[j] > array[j + 1]) {
+          var temp = array[j];
+          array[j] = array[j + 1];
+          array[j + 1] = temp;
+        }
+      }
+    }  return array;
+}
+ 
+bubbleSort([6000, 34, 203, 3, 746, 200, 984, 198, 764, 9, 1]);  // 66 iterations
+bubbleSort2([6000, 34, 203, 3, 746, 200, 984, 198, 764, 9, 1]);  // 77 iterations
