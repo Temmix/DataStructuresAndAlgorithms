7bab7befc7d76e9a073f7a2404a6534786bd9870
Temi Makinde <Temi.Makinde@Gmail.com>
2019-02-03 19:34:38 +0000

    generateCombination

--- Combination/generateCombination.js
+++ Combination/generateCombination.js
@@ -6,3 +6,5 @@ function generateCombination(list){
         return res.concat(arr.length > 1 && arr.slice(0, key).concat(arr.slice(key + 1)).reduce(permute, []).map(function(perm) { return [item].concat(perm); }) || item);
     }, []);
 }
+
+console.log(generateCombination([1,2,3,4]));
