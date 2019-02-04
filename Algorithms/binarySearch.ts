d4b28a6c65fcc41675fca1459a257b284f568a53
Temi Makinde <temi.makinde@gmail.com>
2019-02-04 15:00:28 +0000

    Update binarySearch.ts

--- Algorithms/binarySearch.ts
+++ Algorithms/binarySearch.ts
@@ -1,5 +1,13 @@
-// Big O Notation : O(log n) 
-
+/* Big O Notation : O(log n) 
+     Binary search compares the target value to the middle element of the array. 
+     If they are not equal, the half in which the target cannot lie is eliminated 
+     and the search continues on the remaining half, again taking the middle element 
+     to compare to the target value, and repeating this until the target value is found. 
+     If the search ends with the remaining half being empty, the target is not in the array. 
+     Even though the idea is simple, implementing binary search correctly requires attention 
+     to some subtleties about its exit conditions and midpoint calculation, particularly if 
+     the values in the array are not all of the whole numbers in the range.
+*/
 
 function binarySearch(numArray, key) {
     var middleIdx = Math.floor(numArray.length / 2);
