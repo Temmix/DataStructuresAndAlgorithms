9ae4fd80385d11daafb931d5e69bf67b0808669f
Temi Makinde <temi.makinde@gmail.com>
2019-02-04 15:14:28 +0000

    Update sieveOfEratosthenes.ts

--- Algorithms/sieveOfEratosthenes.ts
+++ Algorithms/sieveOfEratosthenes.ts
@@ -1,3 +1,10 @@
+/*
+  Sieve of Eratosthenes: algorithm steps for primes below 121 
+  (including optimization of starting from prime's square).
+  In mathematics, the sieve of Eratosthenes is a simple, ancient 
+  algorithm for finding all prime numbers up to any given limit.
+*/
+
 function sieveOfEratosthenes(n) {
   const primes = new Array(n).fill(true); 
   primes[0] = false;
