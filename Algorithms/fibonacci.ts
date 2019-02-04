d5094202846ef6705fac36d50da191cb8a9d9885
Temi Makinde <Temi.Makinde@Gmail.com>
2019-02-04 18:59:23 +0000

    merged

--- Algorithms/fibonacci.ts
+++ Algorithms/fibonacci.ts
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-// Big O Notation: O(2^n) : very bad runtime complexity
-// for major improvement , look into Memoization of fibonacci .
-// [0,1,1,2,3,5,8,13,21,34,55,89]
-
-=======
 /* 
   Big O Notation: O(2^n) : very bad runtime complexity
   for major improvement , look into Memoization of fibonacci .
@@ -11,7 +5,6 @@
   called the Fibonacci sequence, such that each number is the sum of the two preceding ones,
   starting from 0 and 1
 */
->>>>>>> 51034ad76b7bad47a6dce1ce061ed1c46695c6b2
 function fib(n){
   return (n < 3) ? 1 : fib(n - 1) + fib(n - 2);
 }
