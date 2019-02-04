9774cc82604df5ceda6fd6f1b2c9c53d16e194f9
Temi Makinde <temi.makinde@gmail.com>
2019-02-04 15:05:26 +0000

    Update fibonacciMemoized.ts

--- Algorithms/fibonacciMemoized.ts
+++ Algorithms/fibonacciMemoized.ts
@@ -1,4 +1,9 @@
-// Big Notation: O(n)
+/* 
+  Big Notation: O(n)
+  In mathematics, the Fibonacci numbers, commonly denoted Fn form a sequence, 
+  called the Fibonacci sequence, such that each number is the sum of the two preceding ones, 
+  starting from 0 and 1
+*/
 
 // using Array as cache
 function fibMemo(index, cache) {
