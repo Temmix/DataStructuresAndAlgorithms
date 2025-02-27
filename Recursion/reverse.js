4b98c4bd112fbf0f584b436bda63ea4f6f46224c
Temi Makinde <temi.makinde@gmail.com>
2025-02-27 22:44:32 +0000

    Update reverse.js

--- Recursion/reverse.js
+++ Recursion/reverse.js
@@ -1,4 +1,24 @@
 //Implement a function that reverses a string using iteration...and then recursion!
+/*
+Reverse
+Write a recursive function called reverse 
+which accepts a string and returns a new string in reverse.
+*/
+
+function reverse(){
+    const A = arguments[0] // for non arrow function.
+
+    function helper(l){
+        if (l < 0) return '';
+        return A[l] + helper(l - 1)
+    }
+    
+   return helper(A.length - 1)  
+}
+
+// reverse('awesome') // 'emosewa'
+// reverse('rithmschool') // 'loohcsmhtir'
+
 function reverseString(str) {
     if(str.length === 1) return str; 
     let n = str.split(''); 
