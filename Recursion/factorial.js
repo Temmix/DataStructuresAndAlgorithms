dfec3cb6445b30d13e898a1e5939d46e915ab187
Temi Makinde <temi.makinde@gmail.com>
2025-02-27 22:37:34 +0000

    Update factorial.js

--- Recursion/factorial.js
+++ Recursion/factorial.js
@@ -2,6 +2,11 @@ function factorialByRecursion(value) {
     return value === 2 ? value : value * factorialByRecursion(value - 1);
 }
 
+function factorial(A){
+   if (A <= 1) return 1 
+   return A * factorial(A - 1)
+}
+
 function factorialByIteration(value){
     let product = 1;
     for(let i = 2; i <= value; i++){
