2173feeef3465a6005e5d1aa4d8444aae8671f37
Temi Makinde <temi.makinde@gmail.com>
2025-03-11 20:16:21 +0000

    Update queue.js

--- Queue/queue.js
+++ Queue/queue.js
@@ -1,3 +1,54 @@
+// FIFO (First In, First out)
+class Node {
+    constructor(value){
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class Queue {
+    constructor(){
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+    
+    enqueue(val){
+        var newNode = new Node(val);
+        if(!this.first){
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            this.last.next = newNode;
+            this.last = newNode;
+        }
+        return ++this.size;
+    }
+
+    dequeue(){
+        if(!this.size) return null
+        
+        const first = this.first
+        if(this.first === this.last) {
+            this.last = first.next
+        }
+        this.first = first.next
+        first.next = null
+        this.size--
+        return first.value
+    }
+}
+
+var queue = new Queue();
+queue.enqueue(10);    // returns 1, size becomes 1
+queue.enqueue(100);   // returns 2, size becomes 2
+queue.enqueue(1000);  // returns 3, size becomes 3
+queue.dequeue(); 
+
+
+
+// ==================================================================================================
+
 class Queue {
     constructor(){
         this.first = null;
